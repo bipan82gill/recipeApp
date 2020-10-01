26126ca61486f58a203ced0da7df2bd1794d3d22
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-10-01 17:53:58 -0400

    get random meal through api

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-async function getRandomMeal(){
-   const randomMeal = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+const meals = document.getElementById('meals');
 
+getRandomMeal();
+
+async function getRandomMeal() {
+   
+    const resp = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+   
+    const respData = await resp.json();
+    const randomMeal = respData.meals[0];
+   
+    console.log(randomMeal);
+
+    addMeal(randomMeal, true);
 }
 async function getMealById() {
     const meal = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i="+id);
@@ -8,4 +19,27 @@ async function getMealById() {
 }
 async function getMealBySearch() {
     const meals= await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s="+term);
+}
+ function addMeal(mealData, random = false){
+    const meal = document.createElement('div');
+    meal.classList.add('meal');
+    meal.innerHTML =`
+        <div class="meal-header">
+        ${random ? `
+        <span class="random">
+                            Random Recipe
+        </span>`:''}
+            
+        <img src="${mealData.strMealThumb}" 
+             alt ="${mealData.strMeal}">
+        </div>
+        <div class="meal-body">
+                    <h4>${mealData.strMeal}</h4>
+                    <button class ="fav-btn">
+                        <i class="fas fa-heart"></i>
+                    </button>
+        </div>
+        
+    `;
+    meals.appendChild(meal);
 }
